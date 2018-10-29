0ef712ac4b449e40600bb10e3d83ca8b18533d34
HsiangCheng <407352024@qq.com>
2018-10-29 18:06:26 +0800

    页面完善部分细节，准备完善api

--- js/api.js
+++ js/api.js
@@ -1,12 +1,5 @@
 var apiBasePath = 'http://localhost:10001/data/addBook'
 
-var BookType = {
-    Literature: {
-        typeCode: 1,
-        description: '文学'
-    }
-}
-
 var bookTypeSet = [
     '哲学'
     ,'文学'
@@ -16,22 +9,34 @@ var bookTypeSet = [
     ,'科技'
 ]
 
+// 添加书籍
 var addBookApi = {
-    basePath: apiBasePath,
-    path: "/addBook",
-    fullPath: this.basePath + this.path,
-    call: function (bookInfo, sucBackCall) {
-        $.post(this.fullPath, function(data) {
-            console.log(data);
-        })
+    basePath: apiBasePath
+    ,path: "/index.html"
+    ,fullPath: this.basePath + this.path
+    ,call: function (query, sucBackCall) {
+        var suc = sucBackCall ? sucBackCall : function(){};
+        $.get(this.fullPath, query, function(data, status) {
+            // console.log(data);
+            if (status == 'success' || success == 'notmodified') {
+                suc(data);
+            } else {
+                _onFail(data, status);
+            }
+        });
+        this._onDone(query);
     }
+    ,onFail: function(func) {this._onFail = func; return this;}
+    ,_onFail: function(data, status){}
+    ,onDone: function(func) {this._onDone = func; return this;}
+    ,_onDone: function(bookInfo){}
 }
 
 var deleteBookApi = {
-    basePath: apiBasePath,
-    path: "/deleteBook",
-    fullPath: this.basePath + this.path,
-    call: function (bookId) {
+    basePath: apiBasePath
+    ,path: "/deleteBook"
+    ,fullPath: this.basePath + this.path
+    ,call: function (bookId) {
         $.post(this.fullPath, function(data) {
             console.log(data);
         })
@@ -39,23 +44,43 @@ var deleteBookApi = {
 }
 
 var editBookApi = {
-    basePath: apiBasePath,
-    path: "/editBook",
-    fullPath: this.basePath + this.path,
-    call: function (bookInfo) {
+    basePath: apiBasePath
+    ,path: "/editBook"
+    ,fullPath: this.basePath + this.path
+    ,call: function (bookInfo) {
         $.post(this.fullPath, function(data) {
             console.log(data);
         })
     }
 }
 
-var selectAllBookApi = {
-    basePath: apiBasePath,
-    path: "/selectAllBook",
-    fullPath: this.basePath + this.path,
-    call: function (bookType) {
-        $.post(this.fullPath, function(data) {
-            console.log(data);
-        })
-    }
-}
+// 书籍搜索，该API与layui紧耦合，只能指定路径
+// 可能会有的参数：
+// bookType：图书类型（int）
+// bookName：图书名字（String）
+// page：第几页
+// limit：一页多少条数据
+// 要求的响应的数据结构：
+// {
+// 	"code": 0,
+// 	"msg": "",
+// 	"count": 1000,
+// 	"data": [{}]
+// }
+// code：0为正常
+// msg：发生异常时提示用的消息
+// count：数据总数量（用于表格分页）
+// data：结果数据集
+var searchBookUrl = 'data/book.json';
+
+// var searchBookApi = {
+//     basePath: apiBasePath,
+//     path: "/selectAllBook",
+//     fullPath: this.basePath + this.path,
+//     call: function (query) {
+//         // query成员
+//         $.post(this.fullPath, function(data) {
+//             console.log(data);
+//         })
+//     }
+// }
---
+++ js/common.js
@@ -0,0 +1,42 @@
+var bookTypes = [
+    { 
+        typeCode: 0,
+        description: '哲学'
+    },
+    {
+        typeCode: 1,
+        description: '文学'
+    },
+    {
+        typeCode: 2,
+        description: '经管'
+    },
+    {
+        typeCode: 3,
+        description: '人文'
+    },
+    {
+        typeCode: 4,
+        description: '计算机'
+    },
+    {
+        typeCode: 5,
+        description: '科技'
+    }
+]
+
+// 初始化
+layui.use(['form'], function() {
+    var form = layui.form;
+    function initSearchBookTypeSelect() {
+        var selectType = $('#searchBookType');
+        bookTypes.forEach(function(value, index, array){
+            var option = $("<option>").val(value.typeCode).text(value.description);
+            selectType.append(option);
+            });
+        form.render('select', 'searchBookForm');
+    }
+    
+    initSearchBookTypeSelect();    
+});
+
